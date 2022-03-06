a2eab98c31f574fb7b538977a2a9710a78b6382f
Puneet <puneet@gmail.com>
2022-03-06 23:54:21 +0530

    first update

---
+++ src/javaapplication15/JavaApplication15.java
@@ -0,0 +1,17 @@
+package javaapplication15;
+
+/**
+ * This class +++ Insert class description here +++
+ *
+ * @author Roopamjit Walia
+ */
+public class JavaApplication15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
